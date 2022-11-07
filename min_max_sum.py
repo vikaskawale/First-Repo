7cb4a3922836a342396eef753d06627b87d72cd5
vikaskawale <100497780+vikaskawale@users.noreply.github.com>
2022-11-08 10:26:58 +1100

    Update min_max_sum.py

--- min_max_sum.py
+++ min_max_sum.py
@@ -3,6 +3,7 @@
 Example
 arr = [1, 3, 5, 7, 9]
 The minimum sum is 1+3+5+7 = 16 and the maximum sum is 3+5+7+9 = 24 . The function prints 16 24'''
+# By Vikas Kawale
 
 def miniMaxSum(arr):
     # Write your code here
