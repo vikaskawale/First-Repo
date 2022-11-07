558148c57971453ecdb1c6b5e59b21ce3daf2af7
Vikas Kawale <vikaskawale15@gmail.com>
2022-11-08 10:18:12 +1100

    first commit

---
+++ reverse_order.py
@@ -0,0 +1,7 @@
+'''Write a Python program which accepts the user's first and last name and print them in reverse order with a 
+space between them.'''
+
+first_name = input('Enter first name: ')
+last_name = input('Enter last name: ')
+
+print('Name in reverse order is: ', last_name,  first_name)
